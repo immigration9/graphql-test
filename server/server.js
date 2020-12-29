be8fdad46e6ad43b1c0bff1179b1b4fb7c15ef07
immigration9 <projgotham18@gmail.com>
2020-12-29 09:54:21 +0900

    initial commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+
+const app = express();
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    graphiql: true,
+    schema,
+  })
+);
+
+app.listen(4000, () => {
+  console.log('Listening to Server on port 4000');
+});
